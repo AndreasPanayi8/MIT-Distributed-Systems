e448ac0106c14d37b43ab5cf2837fbecbc0881ff
AndreasPanayi8 <andreas030508@gmail.com>
2026-08-16 14:24:39 +0300

    rpc.go file done

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -1,23 +1,37 @@
 package mr
 
-//
 // RPC definitions.
 //
 // remember to capitalize all names.
-//
-
 //
 // example to show how to declare the arguments
 // and reply for an RPC.
 //
 
-type ExampleArgs struct {
-	X int
-}
-
-type ExampleReply struct {
-	Y int
-}
-
 // Add your RPC definitions here.
 
+type TaskType int
+
+const (
+	MapTask TaskType = iota
+	ReduceTask
+	WaitTask
+	ExitTask
+)
+
+type GetTaskArgs struct{}
+
+type GetTaskReply struct {
+	Type    TaskType
+	TaskID  int
+	File    string
+	NReduce int
+	NMap    int
+}
+
+type ReportArgs struct {
+	Type   TaskType
+	TaskID int
+}
+
+type ReportReply struct{}
